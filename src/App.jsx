5a4bb234b76448d20bd9418e26335735014d48ca
Dkyadav10 <dishantyadav839@gmail.com>
2025-03-19 14:51:12 +0530

    4 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import HeroSection from "./Components/HeroSection";
+import FeatureSection from './Components/FeatureSection';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
       </div>
+      <FeatureSection />
      
     </>
   )
